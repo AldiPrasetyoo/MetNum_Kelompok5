214ef08c80c7dfdae3061a250f7aa0de8498136c
AldiPrasetyoo <127328037+AldiPrasetyoo@users.noreply.github.com>
2023-09-19 07:50:33 +0700

    Update Nomer1a.py

--- Aldi Prasetyo Widodo/Nomer1a.py
+++ Aldi Prasetyo Widodo/Nomer1a.py
@@ -3,7 +3,7 @@ import numpy as np          # mendeklarasikan mengimpor library numpy untuk digu
 def f(x):                   # mendefinisikan sebuah fungsi dengan nama f(x)
     return x**3 - 2*x + 1   # bagian dari fungsi untuk penghitungan, return digunakan untuk mengembalikan nilai sebagai hasil dari fungsi saaat digunakan atau dipanggil
 
-def bagiDua(f, a, b, tolerGalat):                                   # mendefinisikan sebuah fungsi dengan nama bagiDua(f,a,b,tolerGalat)
+def bagiDua(f, a, b, tolerGalat):                                   # mendefinisikan sebuah fungsi dengan nama bagiDua degan parameter (f,a,b,tolerGalat)
     if np.sign(f(a)) == np.sign(f(b)):                              # pengecekan kondisi fungsi a dan b dengan np.sigm berfunsi sebagai pengambil tanda dari fungsi tersebut, 
         raise Exception('Tidak ada akar pada interval a dan b')     # ketika tanda minus atau negatif dari kedua fungsi tersebut sama maka akan muncul "tidak ada akar" karena dalam akar tidak boleh memiliki tanda yg sama
     while (b - a) / 2 > tolerGalat:                                 # melakukan pengulangan hingga (b-a)/2 lebih besar dari tolerGalat dan sebagai proses pencarian akar dengan metode bagi dua
@@ -21,4 +21,4 @@ a = -2                                   # batas interval bawah
 b = 2                                    # batas interval atas
 tolerGalat = 1e-3                        # toleransi galat yang diperbolehkan
 akar = bagiDua(f, a, b, tolerGalat)      # rumus pencarian akar
-print(f"Akarnya adalah {akar:.3f}")      # mencetak hasil keluaran dengan mengambil hasil dari rumus pencarian akar di atas
+print(f"Akarnya adalah {akar:.3f}")      # mencetak hasil keluaran dengan mengambil hasil dari rumus pencarian akar di atas dengan hasil 3 angka di belakang koma
